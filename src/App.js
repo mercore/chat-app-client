885c2ee25006b78e8a2625421a8773cc9e53570a
IonutRealDevs <ionut@real-devs.com>
2020-09-20 15:09:08 +0300

    emite when app start to wakeup server on heroku

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Join from './views/Join';
 import Chat from './views/Chat';
+import io from 'socket.io-client';
+import config from './utils/config';
 
-const App = () => (
-  <BrowserRouter>
-    <Route path="/" exact component={ Join } />
-    <Route path="/chat" component={ Chat } />
-  </BrowserRouter>
-)
+var socket
+
+const App = () => {
+
+  // this emit is just to wakeup server on heroku
+  useEffect(() => {
+    socket = io(config.SERVER)
+    socket.emit('start-server')
+    socket.on('start-server', () => {
+      console.log('Starting server...')
+    })
+  })
+
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={ Join } />
+      <Route path="/chat" component={ Chat } />
+    </BrowserRouter>
+  )
+}
 
 export default App
